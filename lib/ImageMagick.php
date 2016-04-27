3b43b15b8065bb00331263db97aa73cc1848c6cc
root <root@localhost.localdomain>
2016-04-27 09:57:29 +0800

    imagick class to operate with images

--- lib/ImageMagick.php
+++ lib/ImageMagick.php
@@ -1,13 +1,25 @@
 <?php
 
 class ImageMagic{
+    /**
+     * @var Imagick
+     */
     public $image;
+
+    /**
+     * ImageMagic constructor.
+     * @param $file
+     */
     public function __construct($file)
     {
         $this->image = new Imagick($file);
         return $this->image;
     }
 
+    /**
+     * 获取图片尺寸
+     * @return array
+     */
     public function getSize()
     {
         $wh = [];
@@ -17,7 +29,7 @@ class ImageMagic{
     }
 
     /**
-     * display image
+     * 显示图片
      */
     public function show(){
         header("Content-Type:{$this->image->getImageMimeType()}");
@@ -25,6 +37,12 @@ class ImageMagic{
         echo $data;
     }
 
+    /**
+     * 复制图片
+     * @param string $dir
+     * @param string $fileName
+     * @return mixed
+     */
     public function copy($dir='',$fileName=''){
         /**
          * two ways
@@ -34,16 +52,30 @@ class ImageMagic{
         $fileName = $fileName ? $fileName : $this->genFileName();
         $format = strtolower($this->image->getImageFormat());
         $fileName = $fileName.'.'.$format;
+        $dir = rtrim($dir,'/').'/';
+        $realFileName = $dir.$fileName;
+        //origin file_put_contents
+        /*
         $blobData = $this->image->getImageBlob();
-        $result = file_put_contents($dir.'/'.$fileName,$blobData);
-        //$result = $this->image->writeImage('/tmp/a.jpg');
-        return $result;
+        $result = file_put_contents($realFileName,$blobData);
+        */
+        //writeImage
+        $result = $this->image->writeImage($realFileName);
+        $storage['fileName'] = $fileName;
+        $storage['flag'] = $result ? true : false;
+        return $storage;
     }
 
+    /**
+     * 获取随机文件名
+     * @return string
+     */
     public function genFileName(){
         return date('YmdHis').uniqid();
     }
+
 }
 
-$img = new ImageMagic('imgs/girl.jpg');
-$img->copy();
+/*$img = new ImageMagic('imgs/girl.jpg');
+$result = $img->copy('./tmp','test');
+var_dump($result);*/
